8b8c33933f1923fa50d39c6719011ef4028e3b53
arosh.segar <arosh.segar@gmail.com>
2024-07-28 22:56:29 +0530

    changes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
 import { AuthProvider } from "./auth";
 import Dashboard from "./components/dashboard/Dashboard";
@@ -48,6 +53,7 @@ function App() {
 
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/login" replace={true} />} />
         </Routes>
       </AuthProvider>
     </Router>
